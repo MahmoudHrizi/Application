677327f9eb1ebb980c9f23e62c906dda7a7c2d87
MahmoudHrizi <mahmoudhrizi40@gmail.com>
2021-03-04 21:54:39 +0100

    MyApplications

---
+++ corbeille.js
@@ -0,0 +1,35 @@
+<header id="hero" data-w-id="" class="section hero slider">
+
+
+    <div data-deay="15000" data-animation="slide" data-autoplay="1" data-duration="500" data-infinite="1" className="fh-slider w-slider"  role="region" aria-label="carousel">
+    <div className="w-slider-mask" id="w-slider-mask-0">
+      <div className="hero-slide-1 time vbg w-slide" aria-label="1of 3" role="group" /*style={"transform: translateX(-453.25px); opacity: 1; transition: transform 500ms ease 0s;"}*/ aria-hidden="true">
+      <div class="slide-content-wrapper time-2">
+        <div data-poster-url="https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fdaf36da3c5a386f5abe92f_Site-BG-Hero-Video-final-2-poster-00001.jpg" data-video-urls="https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fdaf36da3c5a386f5abe92f_Site-BG-Hero-Video-final-2-transcode.mp4,https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fdaf36da3c5a386f5abe92f_Site-BG-Hero-Video-final-2-transcode.webm" data-autoplay="true" data-loop="true" data-wf-ignore="true" class="background-video w-background-video w-background-video-atom">
+          <video autoplay="" loop="" /*style={'background-image:url(&quot;https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fdaf36da3c5a386f5abe92f_Site-BG-Hero-Video-final-2-poster-00001.jpg&quot;)'}*/ muted="" playsinline="" data-wf-ignore="true" data-object-fit="cover" aria-hiden="true">
+          <source src="https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fdaf36da3c5a386f5abe92f_Site-BG-Hero-Video-final-2-transcode.webm" data-wf-ignore="true"></source>
+          <source src="https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fdaf36da3c5a386f5abe92f_Site-BG-Hero-Video-final-2-transcode.webm" data-wf-ignore="true" aria-hidden="true"></source>
+          </video>
+        </div>
+        <div className="slide-content-wrapper">
+          <div className="moon-ultra-wrapper tab-mobile hide">
+          <img src="https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fd01dbcd387a1e9defab14e_MOON_UltraLight_Full_MOON_Epic.png" loading="lazy" sizes="100vw" srcset="https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fd01dbcd387a1e9defab14e_MOON_UltraLight_Full_MOON_Epic-p-500.png 500w, https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fd01dbcd387a1e9defab14e_MOON_UltraLight_Full_MOON_Epic.png 800w" alt="" class="moon-ultralight-img two" aria-hidden="true"/>
+
+          </div>
+          <div className="time-copy" aria-hidden="true">
+          <img src="https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fd01dbcd387a17d91fab17c_Time_Magazine_logo%402x.png" loading="lazy" sizes="(max-width: 479px) 160px, (max-width: 767px) 220px, (max-width: 991px) 300px, 350px" srcset="https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fd01dbcd387a17d91fab17c_Time_Magazine_logo%25402x-p-500.png 500w, https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fd01dbcd387a17d91fab17c_Time_Magazine_logo%25402x-p-800.png 800w, https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fd01dbcd387a17d91fab17c_Time_Magazine_logo%25402x-p-1080.png 1080w, https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fd01dbcd387a17d91fab17c_Time_Magazine_logo%25402x-p-1600.png 1600w, https://assets-global.website-files.com/5fd01dbcd387a144a2fab08d/5fd01dbcd387a17d91fab17c_Time_Magazine_logo%402x.png 1801w" alt="" class="time-logo" aria-hidden="true"/>
+          <h1 class="h1 hero time" aria-hidden="true">Best Inventions 2020</h1>
+          </div>
+           
+
+         </div>
+
+      </div>
+
+    </div>
+
+    
+
+    </div>
+    </div>
+  </header>
